74de99fdc902efe1bf0be6d1758cadda1e15beb2
jcarbaugh7 <carbaugh@bu.edu>
2021-10-08 18:09:20 -0400

    write visualization function

--- visualization_function.R
+++ visualization_function.R
@@ -1,9 +1,14 @@
 
+library(tidyverse)
 
+bulild_visual <- function(tib){
+  # 
+  # tib_early <- filter(tib, year <1920)
+  # tib_late <- filter(tib, year >=1920)
+  # 
+  visual <- ggplot(data = tib) +
+    geom_point(mapping = aes(x = log(Emissions), y = Life_Expectancy, color = country), show.legend = FALSE)
+  
+  return(visual)
+}
 
-
-joined_early <- filter(joined, year <1920)
-joined_late <- filter(joined, year >=1920)
-
-ggplot(data = joined_early)+
-  geom_point(mapping = aes(x = log(Emissions), y = Life_Expectancy, color = country), show.legend = FALSE)
